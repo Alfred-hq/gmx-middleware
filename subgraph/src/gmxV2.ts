acc353022a02c0964bca0cba4f39a172a8cf3d8f
Akshay Lalwani <alalwani@deqode.com>
2023-09-27 13:33:53 +0530

    fix: removed unused event handler

--- subgraph/src/gmxV2.ts
+++ subgraph/src/gmxV2.ts
@@ -15,49 +15,13 @@ import { DecreasePositionV2, feeV2, PositionSettledV2, PositionSlotV2, IncreaseP
 import { EventLog1 } from "../generated/EventEmitter/EventEmitter";
 
 
-export function handleEventLog(event: EventLogEvent): void {
-  log.info("event log",[])
-  return
-}
+// export function handleEventLog(event: EventLogEvent): void {
+//   log.info("event log",[])
+//   return
+// }
 
 export function handleEventLog1(event: EventLog1Event): void {
   log.info("event log 1",[])
-  // let entity = new EventLog1(
-  //   event.transaction.hash.concatI32(event.logIndex.toI32())
-  // )
-  // entity.msgSender = event.params.msgSender
-  // entity.eventName = event.params.eventName
-  // entity.eventNameHash = event.params.eventNameHash
-  // entity.topic1 = event.params.topic1
-  // entity.eventData_addressItems_items =
-  //   event.params.eventData.addressItems.items
-  // entity.eventData_addressItems_arrayItems =
-  //   event.params.eventData.addressItems.arrayItems
-  // entity.eventData_uintItems_items = event.params.eventData.uintItems.items
-  // entity.eventData_uintItems_arrayItems =
-  //   event.params.eventData.uintItems.arrayItems
-  // entity.eventData_intItems_items = event.params.eventData.intItems.items
-  // entity.eventData_intItems_arrayItems =
-  //   event.params.eventData.intItems.arrayItems
-  // entity.eventData_boolItems_items = event.params.eventData.boolItems.items
-  // entity.eventData_boolItems_arrayItems =
-  //   event.params.eventData.boolItems.arrayItems
-  // entity.eventData_bytes32Items_items =
-  //   event.params.eventData.bytes32Items.items
-  // entity.eventData_bytes32Items_arrayItems =
-  //   event.params.eventData.bytes32Items.arrayItems
-  // entity.eventData_bytesItems_items = event.params.eventData.bytesItems.items
-  // entity.eventData_bytesItems_arrayItems =
-  //   event.params.eventData.bytesItems.arrayItems
-  // entity.eventData_stringItems_items = event.params.eventData.stringItems.items
-  // entity.eventData_stringItems_arrayItems =
-  //   event.params.eventData.stringItems.arrayItems
-
-  // entity.blockNumber = event.block.number
-  // entity.blockTimestamp = event.block.timestamp
-  // entity.transactionHash = event.transaction.hash
-
-  // entity.save()
   const eventName = event.params.eventName;
   const isFeeEvent = eventName == "PositionFeesCollected"
   const isIncEvent = eventName == "PositionIncrease"
@@ -81,8 +45,8 @@ export function handleEventLog1(event: EventLog1Event): void {
   return
 }
 
-export function handleEventLog2(event: EventLog2Event): void {
-  log.info("event log 2",[])
-  return
-}
+// export function handleEventLog2(event: EventLog2Event): void {
+//   log.info("event log 2",[])
+//   return
+// }
 
