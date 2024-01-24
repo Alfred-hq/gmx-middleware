206d7590d44ae3476283a060ebd81dcbfeab4a35
jpatidar30 <jpatidar@deqode.com>
2024-01-24 16:08:24 +0530

    feat: capture v2 order events and save to subgrapgh

--- subgraph/src/gmxV2.ts
+++ subgraph/src/gmxV2.ts
@@ -4,82 +4,121 @@ import {
   EventLog1EventDataStruct,
   EventLog2 as EventLog2Event,
   EventLogEventDataStringItemsArrayItemsStruct,
-  EventLogEventDataStruct
-} from "../generated/EventEmitter/EventEmitter"
+  EventLogEventDataStruct,
+} from "../generated/EventEmitter/EventEmitter";
 import { handleDecreasePositionEventV2 } from "./decreasePosition";
-import { EventData } from "./EventEmitter"
+import { EventData } from "./EventEmitter";
 import { handleIncreasePositionEventV2 } from "./increasePosition";
-import { log } from '@graphprotocol/graph-ts'
+import { log } from "@graphprotocol/graph-ts";
 import { handleFeeEventV2 } from "./gmxV2Fee";
-import { DecreasePositionV2, feeV2, PositionSettledV2, PositionSlotV2, IncreasePositionV2, TradesV2, Market } from "../generated/schema"
+import {
+  DecreasePositionV2,
+  feeV2,
+  PositionSettledV2,
+  PositionSlotV2,
+  IncreasePositionV2,
+  TradesV2,
+  Market,
+} from "../generated/schema";
 import { EventLog1 } from "../generated/EventEmitter/EventEmitter";
 import { handleOraclePriceUpdateEvent } from "./gmxv2Prices";
 import { returnAddressOrZeroAddress } from "./common";
-
-
+import { handleOrderCreatedEventV2, handleOrderCancelledEventV2, handleOrderUpdatedEventV2 } from "./ordersV2";
 // export function handleEventLog(event: EventLogEvent): void {
 //   log.info("event log",[])
 //   return
 // }
 
 export function handleEventLog1(event: EventLog1Event): void {
-  log.info("event log 1",[])
+  log.info("event log 1", []);
   const eventName = event.params.eventName;
-  const isFeeEvent = eventName == "PositionFeesCollected"
-  const isIncEvent = eventName == "PositionIncrease"
-  const isDecEvent = eventName == "PositionDecrease"
-  const isPriceEvent = eventName == "OraclePriceUpdate"
-  const isMarketCreatedEvent = eventName == "MarketCreated"
+  const isFeeEvent = eventName == "PositionFeesCollected";
+  const isIncEvent = eventName == "PositionIncrease";
+  const isDecEvent = eventName == "PositionDecrease";
+  const isPriceEvent = eventName == "OraclePriceUpdate";
+  const isMarketCreatedEvent = eventName == "MarketCreated";
+  const isOrderCreatedEvent = eventName == "OrderCreated";
+  const isOrderUpdatedEvent = eventName == "OrderUpdated";
+  const isOrderCancelledEvent = eventName == "OrderCancelled";
 
-  const data = new EventData(event.params.eventData)
-  if (!isFeeEvent && !isIncEvent && !isDecEvent && !isPriceEvent && !isMarketCreatedEvent) {
-    return
+  const data = new EventData(event.params.eventData);
+  if (
+    !isFeeEvent &&
+    !isIncEvent &&
+    !isDecEvent &&
+    !isPriceEvent &&
+    !isMarketCreatedEvent &&
+    !isOrderCreatedEvent &&
+    !isOrderUpdatedEvent &&
+    !isOrderCancelledEvent
+  ) {
+    return;
   }
-  if(isIncEvent){
-    log.debug("increase event ",[])
-      handleIncreasePositionEventV2(event,data)
+  if (isIncEvent) {
+    log.debug("increase event ", []);
+    handleIncreasePositionEventV2(event, data);
   }
-  if(isDecEvent){
-      log.debug("decrease event ",[])
-      handleDecreasePositionEventV2(event,data)
+  if (isDecEvent) {
+    log.debug("decrease event ", []);
+    handleDecreasePositionEventV2(event, data);
   }
-  if(isFeeEvent){
-    log.debug("fee event ",[])
-    handleFeeEventV2(event,data)
+  if (isFeeEvent) {
+    log.debug("fee event ", []);
+    handleFeeEventV2(event, data);
   }
-  if(isPriceEvent){
-    log.debug("price event",[])
-    handleOraclePriceUpdateEvent(event,data)
+  if (isPriceEvent) {
+    log.debug("price event", []);
+    handleOraclePriceUpdateEvent(event, data);
   }
-  if(isMarketCreatedEvent){
-    log.debug("market event",[])
-    handleMarketCreatedEvent(event,data)
+  if (isMarketCreatedEvent) {
+    log.debug("market event", []);
+    handleMarketCreatedEvent(event, data);
   }
-  return
+  if (isOrderCreatedEvent) {
+    log.debug("order created event ", []);
+    handleOrderCreatedEventV2(event, data);
+  }
+  if (isOrderUpdatedEvent) {
+    log.debug("order updated event ", []);
+    handleOrderUpdatedEventV2(event, data);
+  }
+  if (isOrderCancelledEvent) {
+    log.debug("order cancelled event ", []);
+    handleOrderCancelledEventV2(event, data);
+  }
+  return;
 }
 
-export function handleMarketCreatedEvent(event: EventLog1,data: EventData): void {
-  //address item 
-//   marketToken
-// indexToken
-// longToken
-// shortToken
-const indexToken=returnAddressOrZeroAddress(data.getAddressItemString("indexToken"))
-const marketToken=returnAddressOrZeroAddress(data.getAddressItemString("marketToken"))
-const longToken=returnAddressOrZeroAddress(data.getAddressItemString("longToken"))
-const shortToken=returnAddressOrZeroAddress(data.getAddressItemString("shortToken"))
-const market = new Market(marketToken)
-market.indexToken=indexToken
-market.longToken=longToken
-market.shortToken=shortToken
-market.marketToken=marketToken
-market.save()
-
+export function handleMarketCreatedEvent(
+  event: EventLog1,
+  data: EventData
+): void {
+  //address item
+  //   marketToken
+  // indexToken
+  // longToken
+  // shortToken
+  const indexToken = returnAddressOrZeroAddress(
+    data.getAddressItemString("indexToken")
+  );
+  const marketToken = returnAddressOrZeroAddress(
+    data.getAddressItemString("marketToken")
+  );
+  const longToken = returnAddressOrZeroAddress(
+    data.getAddressItemString("longToken")
+  );
+  const shortToken = returnAddressOrZeroAddress(
+    data.getAddressItemString("shortToken")
+  );
+  const market = new Market(marketToken);
+  market.indexToken = indexToken;
+  market.longToken = longToken;
+  market.shortToken = shortToken;
+  market.marketToken = marketToken;
+  market.save();
 }
-
 
 // export function handleEventLog2(event: EventLog2Event): void {
 //   log.info("event log 2",[])
 //   return
 // }
-
---
+++ subgraph/src/ordersV2.ts
@@ -0,0 +1,119 @@
+import { EventLog1 } from "../generated/EventEmitter/EventEmitter";
+import { OrderV2 } from "../generated/schema";
+import { EventData } from "./EventEmitter";
+import { returnAddressOrZeroAddress } from "./common";
+import { returnValueOrZero } from "./increasePosition";
+
+export function handleOrderCreatedEventV2(
+  event: EventLog1,
+  data: EventData
+): void {
+  const keyBytes32 = data.getBytes32Item("key");
+  if (!keyBytes32) {
+    return;
+  }
+  const key = keyBytes32.toHexString();
+  let Order = new OrderV2(key);
+  Order.key = key;
+  Order.account = returnAddressOrZeroAddress(
+    data.getAddressItemString("account")
+  );
+  Order.receiver = returnAddressOrZeroAddress(
+    data.getAddressItemString("receiver")
+  );
+  Order.uiFeeReceiver = returnAddressOrZeroAddress(
+    data.getAddressItemString("uiFeeReceiver")
+  );
+  Order.market = returnAddressOrZeroAddress(
+    data.getAddressItemString("market")
+  );
+  Order.initialCollateralToken = returnAddressOrZeroAddress(
+    data.getAddressItemString("initialCollateralToken")
+  );
+  Order.orderType = returnValueOrZero(data.getUintItem("orderType"));
+  Order.decreasePositionSwapType = returnValueOrZero(
+    data.getUintItem("decreasePositionSwapType")
+  );
+  Order.sizeDeltaUsd = returnValueOrZero(data.getUintItem("sizeDeltaUsd"));
+  Order.initialCollateralDeltaAmount = returnValueOrZero(
+    data.getUintItem("initialCollateralDeltaAmount")
+  );
+  Order.triggerPrice = returnValueOrZero(data.getUintItem("triggerPrice"));
+  Order.acceptablePrice = returnValueOrZero(
+    data.getUintItem("acceptablePrice")
+  );
+  Order.executionFee = returnValueOrZero(data.getUintItem("executionFee"));
+  Order.callbackGasLimit = returnValueOrZero(
+    data.getUintItem("callbackGasLimit")
+  );
+  Order.minOutputAmount = returnValueOrZero(
+    data.getUintItem("minOutputAmount")
+  );
+  Order.updatedAtBlock = returnValueOrZero(data.getUintItem("updatedAtBlock"));
+
+  Order.isLong = data.getBoolItem("isLong");
+  Order.shouldUnwrapNativeToken = data.getBoolItem("shouldUnwrapNativeToken");
+  Order.isFrozen = data.getBoolItem("isFrozen");
+
+  Order.blockNumber = event.block.number;
+  Order.blockTimestamp = event.block.timestamp;
+  Order.transactionHash = event.transaction.hash.toHexString();
+  Order.logIndex = event.logIndex;
+  Order.isCancelled = false;
+  Order.save();
+  return;
+}
+
+export function handleOrderUpdatedEventV2(
+  event: EventLog1,
+  data: EventData
+): void {
+  const keyBytes32 = data.getBytes32Item("key");
+  if (!keyBytes32) {
+    return;
+  }
+  const key = keyBytes32.toHexString();
+  let Order = OrderV2.load(key);
+  Order.blockNumber = event.block.number;
+  Order.blockTimestamp = event.block.timestamp;
+  Order.transactionHash = event.transaction.hash.toHexString();
+  Order.logIndex = event.logIndex;
+  Order.account = returnAddressOrZeroAddress(
+    data.getAddressItemString("account")
+  );
+  Order.sizeDeltaUsd = returnValueOrZero(data.getUintItem("sizeDeltaUsd"));
+  Order.triggerPrice = returnValueOrZero(data.getUintItem("triggerPrice"));
+  Order.acceptablePrice = returnValueOrZero(
+    data.getUintItem("acceptablePrice")
+  );
+  Order.minOutputAmount = returnValueOrZero(
+    data.getUintItem("minOutputAmount")
+  );
+  Order.save();
+
+  return;
+}
+
+export function handleOrderCancelledEventV2(
+  event: EventLog1,
+  data: EventData
+): void {
+  const keyBytes32 = data.getBytes32Item("key");
+  if (!keyBytes32) {
+    return;
+  }
+  const key = keyBytes32.toHexString();
+  let Order = OrderV2.load(key);
+  Order.blockNumber = event.block.number;
+  Order.blockTimestamp = event.block.timestamp;
+  Order.transactionHash = event.transaction.hash.toHexString();
+  Order.logIndex = event.logIndex;
+  Order.account = returnAddressOrZeroAddress(
+    data.getAddressItemString("account")
+  );
+  Order.isCancelled = true;
+  Order.reason = data.getStringItem("reason");
+  Order.reasonBytes = data.getBytesItem("reasonBytes");
+  Order.save();
+  return;
+}
